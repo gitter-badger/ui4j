1558d32925db76da64ff93003aaa4cae0b943077
Mike Kaufman <MikeKaufman@users.noreply.github.com>
2014-09-09 16:05:41 +0100

    Added Javadoc package-info to com.ui4j.api.browser

---
+++ ui4j-api/src/main/java/com/ui4j/api/browser/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * The main Ui4j API, consisting of interfaces and classes for obtaining and configuring a
+ * "browser engine" and retrieving the content of web pages.
+ */
+package com.ui4j.api.browser;
